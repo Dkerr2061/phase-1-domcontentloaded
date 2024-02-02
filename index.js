e44cd1f619957dd56a913403e3f0d8148288f112
David Kerr <kerrky@gmail.com>
2024-02-01 23:51:52 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+  let textElement = document.getElementById('text');
+  textElement.innerHTML = 'This is really cool!'
+})
+
